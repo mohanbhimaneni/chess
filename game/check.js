197078f762d783816719b2671fa65aabcd9625c0
mohanbhimaneni <129185293+mohanbhimaneni@users.noreply.github.com>
2024-05-19 17:47:42 +0530

    new feature on 19th may

---
+++ game/check.js
@@ -0,0 +1,10 @@
+function underCheck(placement) {
+    let king=document.getElementsByClassName(boardPlacement[placement])[0];
+    console.log(placement);
+    console.log(king);
+    
+}
+
+function check(placement) {
+    
+}
